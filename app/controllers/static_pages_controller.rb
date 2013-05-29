2ab893826a84407ef83905790123d3cfcdd8f7db
Marvin Qian <fueledbymarvin@gmail.com>
2013-05-29 14:24:50 -0400

    Initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def register
+  end
+
+  def schedule
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itasa::Application.config.secret_token = '0e2cf47ac19f44a5689111a7362b119ccfb792c2a085f0b46b3d4fb95ccdd75b22b1fb93717f606108d644148441bcd4fade6b46ddb9123681a19cab810292ae'
