e0564c2c52fc830d516baccf83ee5c4f82449fd9
Marvin Qian <fueledbymarvin@gmail.com>
2013-07-19 12:36:49 -0400

    Add placeholder for production Facebook omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '343629322437383', '7e510c5b72f620481bf44d5873914161'
+	if Rails.env == "production"
+		provider :facebook, 'YOUR ID', 'YOUR SECRET'
+	else
+		provider :facebook, '343629322437383', '7e510c5b72f620481bf44d5873914161'
+	end
 end
