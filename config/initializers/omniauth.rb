6fc93f40593b014e1d510897e1911a92cfa89069
Marvin Qian <fueledbymarvin@gmail.com>
2013-09-06 23:31:47 -0400

    Modify Facebook permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
 if Rails.env == "production"
-	ENV['fb_app_id'] = "FILL IN" 
-	ENV['fb_secret'] = "FILL IN"
+	ENV['fb_app_id'] = "572459842812961" 
+	ENV['fb_secret'] = "09dd5858d4e06bc66b803f87dbb6dfdc"
 else
-	ENV['fb_app_id'] = '572459842812961'
-	ENV['fb_secret'] = '09dd5858d4e06bc66b803f87dbb6dfdc'
+	ENV['fb_app_id'] = '343629322437383'
+	ENV['fb_secret'] = '7e510c5b72f620481bf44d5873914161'
 end
 
 ENV['fb_permissions'] = "email" # e.g. create_event, user_groups, publish_stream
